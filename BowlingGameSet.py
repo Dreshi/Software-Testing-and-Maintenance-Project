c1c9d2626629375efc33da88448b636cf24cd206
Dreshi <73090979+Dreshi@users.noreply.github.com>
2022-04-27 22:01:45 +1200

    Added Python Documentation to the file

--- BowlingGameSet.py
+++ BowlingGameSet.py
@@ -1,11 +1,41 @@
 class BowlingGame:
+
+    """ Description	"""
+
     def __init__(self):
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
         self.rolls = []
 
     def roll(self, pins):
+        """ Description
+        :type self:
+        :param self:
+
+        :type pins:
+        :param pins:
+
+        :raises:
+
+        :rtype:
+        """
         self.rolls.append(pins)
 
     def score(self):
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
         result = 0
         rollIndex = 0
         for frameIndex in range(10):  # checking the possibility of 10 turns
@@ -23,16 +53,72 @@ class BowlingGame:
         return result
 
     def isStrike(self, rollIndex):
+        """ Description
+        :type self:
+        :param self:
+
+        :type rollIndex:
+        :param rollIndex:
+
+        :raises:
+
+        :rtype:
+        """
+
         return self.rolls[rollIndex] == 10
 
     def isSpare(self, rollIndex):
+        """ Description
+        :type self:
+        :param self:
+
+        :type rollIndex:
+        :param rollIndex:
+
+        :raises:
+
+        :rtype:
+        """
         return self.rolls[rollIndex] + self.rolls[rollIndex+1] == 10
 
     def strikeScore(self, rollIndex):  # method was misspelled
+        """ Description
+        :type self:
+        :param self:
+
+        :type rollIndex:
+        :param rollIndex:
+
+        :raises:
+
+        :rtype:
+        """
         return 10 + self.rolls[rollIndex+1] + self.rolls[rollIndex+2]
 
     def spareScore(self, rollIndex):
+        """ Description
+        :type self:
+        :param self:
+
+        :type rollIndex:
+        :param rollIndex:
+
+        :raises:
+
+        :rtype:
+        """
         return 10 + self.rolls[rollIndex+2]
 
     def frameScore(self, rollIndex):
+        """ Description
+        :type self:
+        :param self:
+
+        :type rollIndex:
+        :param rollIndex:
+
+        :raises:
+
+        :rtype:
+        """
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
--- Test_Main.py
+++ Test_Main.py
@@ -4,19 +4,52 @@ from BowlingGameSet import BowlingGame
 
 class TestBowlingGame(unittest.TestCase):
 
+    """ Description	"""
+
     def setUp(self):
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
         self.game = BowlingGame()
 
     def testGutterGame(self):
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
         self.rollMany(0, 20)  # it was important to use the rollMany() here
         assert self.game.score() == 0
 
     def testAllOnes(self):
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
         self.rollMany(1, 20)
         assert self.game.score() == 20
 
     def testOneSpare(self):  # method name was used twise
-        # in order to not get the IndexError: list index out of range error, it was important to delete the s from rolls()
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """    # in order to not get the IndexError: list index out of range error, it was important to delete the s from rolls()
         self.game.roll(5)
         self.game.roll(5)
         self.game.roll(3)
@@ -24,7 +57,15 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score() == 16
 
     def testOneStrike(self):
-        # in order to not get the IndexError: list index out of range error, it was important to delete the s from rolls()
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """    # in order to not get the IndexError: list index out of range error, it was important to delete the s from rolls()
+
         self.game.roll(10)
         self.game.roll(4)
         self.game.roll(3)
@@ -32,17 +73,55 @@ class TestBowlingGame(unittest.TestCase):
         assert self.game.score() == 24
 
     def testPerfectGame(self):
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
+
         self.rollMany(10, 12)
         assert self.game.score() == 300
 
     def testAllSpare(self):  # naming had to be changed - the method name testOneSpare(self) was used 2x, plus this one was actually checking for all spares
+        """ Description
+        :type self:
+        :param self:
+
+        :raises:
+
+        :rtype:
+        """
+
         self.rollMany(5, 21)
         assert self.game.score() == 150
 
     def rollMany(self, pins, rolls):
+        """ Description
+        :type self:
+        :param self:
+
+        :type pins:
+        :param pins:
+
+        :type rolls:
+        :param rolls:
+
+        :raises:
+
+        :rtype:
+        """
         for i in range(rolls):
             self.game.roll(pins)
 
 
 if __name__ == '__main__':
+
+    """ Description
+    :raises:
+
+    :rtype:
+    """
     unittest.main()
